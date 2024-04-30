b8d9c28eae6677635f4fd88e88a502ced9eb7338
Caroline Arnold <arnold@dkrz.de>
2024-04-30 10:36:40 +0200

    generate experiment submission scripts

---
+++ scripts/gen_exp_scripts.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+
+with open('/home/k/k202141/rootgit/AI4EO-MapYourCity/scripts/header.txt') as f:
+    header = f.readlines()
+
+with open('/home/k/k202141/rootgit/AI4EO-MapYourCity/scripts/expsource.txt') as f:
+    explines = f.readlines()
+
+sroot = '/home/k/k202141/rootgit/AI4EO-MapYourCity/scripts/'
+
+with open(os.path.join(sroot, 'batch.sh'), 'w') as ff:
+    for i, l in enumerate(explines):
+        sf = f'submit_{i:02d}.sh'
+    
+        with open(os.path.join(sroot, sf), 'w') as f:
+            for h in header:
+                f.write(h)
+            f.write(l)
+            print(l)
+    
+            f.close()
+        ff.write(f'sbatch {sf}\n')
+        ff.write('sleep 10\n')
+    ff.close()
